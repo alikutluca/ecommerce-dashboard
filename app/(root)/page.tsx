5e9f0443291786fa988725dbfce0f2d1e3183f65
alikutluca <alikutluca@yahoo.com>
2023-08-15 15:51:18 +0300

    fix: created

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <p>Hello to dashboard</p>
+      <Button size="default" variant="default">
+        Click mE
+      </Button>
+    </div>
+  );
+}
